aff51498fab70d354a13e081f52628ccb3037571
credes-me <ujjwal.1923co1003@kiet.edu>
2022-11-09 22:58:18 +0530

    configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  module.exports = {
+    root: true,
+    extends: '@react-native-community',
+    "rules": {
+      "prettier/prettier": ["error",{
+        "endOfLine": "auto"}
+      ]
+  
+    }
+  },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   overrides: [
